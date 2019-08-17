60e00ae5a289feba60def55a8ab64df845916508
Ben Patterson <bmizepatterson@gmail.com>
2019-08-17 16:55:39 -0400

    Set Paper.js up in ng

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SimComponent } from './sim/sim.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: '', component: SimComponent },
+];
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
--- src/app/sim/sim.component.ts
+++ src/app/sim/sim.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { Project, Path } from 'paper';
+import * as paper from 'paper';
 
 @Component({
-  selector: 'app-sim',
-  templateUrl: './sim.component.html',
-  styleUrls: ['./sim.component.scss']
+    templateUrl: './sim.component.html',
+    styleUrls: ['./sim.component.scss']
 })
 export class SimComponent implements OnInit {
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        window['paper'] = paper;
+
+        const project1 = new Project('sim');
+        const path = new Path.Circle({
+            center: [80, 50],
+            radius: 30,
+            strokeColor: 'black'
+        });
+    }
 
 }
