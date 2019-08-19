06841e88d7782dd96d45cf314fc7c3591550d672
Ben Patterson <ben.patterson@avnet.com>
2019-08-19 12:26:49 -0400

    Draw pointer

--- src/app/sim/sim.component.ts
+++ src/app/sim/sim.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Path, Rectangle, Point, Color, view } from 'paper';
+import { Path, Rectangle, Point, view, Layer } from 'paper';
 import * as paper from 'paper';
 import { West } from '../theme/themes';
 
@@ -11,37 +11,72 @@ export class SimComponent implements OnInit {
 
     gridSize = 50;
 
+    pointer: Path.Circle;
+
+    // Layers
+    backLayer: Layer;
+    pointerLayer: Layer;
+
     ngOnInit() {
         // Setup PaperJs
         paper.install(window);
         paper.setup('sim');
+        this.backLayer = new Layer();
+        this.pointerLayer = new Layer();
+
+        // Route events
         paper.view.onResize = e => this.onResize(e);
+        paper.view.onMouseMove = e => this.onMouseMove(e);
+        paper.view.onMouseLeave = () => this.onMouseLeave();
 
         // Setup workspace
         this.drawBackground();
     }
 
     drawBackground() {
+        this.backLayer.activate();
         const background = new Path.Rectangle(new Rectangle(new Point(0, 0), view.viewSize));
-        background.fillColor = West.platinum;
 
         // Draw gridlines
         for (let y = this.gridSize; y < view.viewSize.height; y += this.gridSize) {
             const vertical = new Path();
             vertical.add(new Point(0, y));
             vertical.add(new Point(view.viewSize.width, y));
-            vertical.strokeColor = West.tuscany;
         }
         for (let x = this.gridSize; x < view.viewSize.width; x += this.gridSize) {
             const horizontal = new Path();
             horizontal.add(new Point(x, 0));
             horizontal.add(new Point(x, view.viewSize.height));
-            horizontal.strokeColor = West.tuscany;
         }
+        this.backLayer.fillColor = West.platinum;
+        this.backLayer.strokeColor = West.tuscany;
+        this.backLayer.dashArray = [this.gridSize / 5, this.gridSize / 10];
+    }
+
+    drawPointer(point: Point) {
+        // Snap pointer to grid
+        point.x = Math.round(point.x / this.gridSize) * this.gridSize;
+        point.y = Math.round(point.y / this.gridSize) * this.gridSize;
+        this.pointerLayer.activate();
+        if (!this.pointer) {
+            this.pointer = new Path.Circle(point, this.gridSize / 10);
+        }
+        this.pointer.position = point;
+        this.pointerLayer.fillColor = West.copperPenny;
+        this.pointerLayer.fillColor.alpha = 0.8;
     }
 
     onResize(event: any) {
         this.drawBackground();
+    }
+
+    onMouseMove(event: any) {
+        this.drawPointer(event.point);
+    }
+
+    onMouseLeave() {
+        this.pointer = null;
+        this.pointerLayer.removeChildren();
     }
 
 }
