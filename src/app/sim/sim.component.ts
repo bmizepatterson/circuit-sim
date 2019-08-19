969f7ca1fd75741d49799779a8bd59312d808d9f
Ben Patterson <ben.patterson@avnet.com>
2019-08-19 11:49:50 -0400

    Draw background with gridlines

--- src/app/sim/sim.component.ts
+++ src/app/sim/sim.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { Path, Rectangle, Point, Color } from 'paper';
+import { Path, Rectangle, Point, Color, view } from 'paper';
 import * as paper from 'paper';
+import { West } from '../theme/themes';
 
 @Component({
     templateUrl: './sim.component.html',
@@ -8,18 +9,39 @@ import * as paper from 'paper';
 })
 export class SimComponent implements OnInit {
 
+    gridSize = 50;
+
     ngOnInit() {
         // Setup PaperJs
         paper.install(window);
         paper.setup('sim');
+        paper.view.onResize = e => this.onResize(e);
 
         // Setup workspace
-        this.setupWorkspace();
+        this.drawBackground();
     }
 
-    setupWorkspace() {
-        const background = new Path.Rectangle(new Rectangle(new Point(0, 0), new Point(100, 100)));
-        background.fillColor = new Color(0, 0, 0);
+    drawBackground() {
+        const background = new Path.Rectangle(new Rectangle(new Point(0, 0), view.viewSize));
+        background.fillColor = West.platinum;
+
+        // Draw gridlines
+        for (let y = this.gridSize; y < view.viewSize.height; y += this.gridSize) {
+            const vertical = new Path();
+            vertical.add(new Point(0, y));
+            vertical.add(new Point(view.viewSize.width, y));
+            vertical.strokeColor = West.tuscany;
+        }
+        for (let x = this.gridSize; x < view.viewSize.width; x += this.gridSize) {
+            const horizontal = new Path();
+            horizontal.add(new Point(x, 0));
+            horizontal.add(new Point(x, view.viewSize.height));
+            horizontal.strokeColor = West.tuscany;
+        }
+    }
+
+    onResize(event: any) {
+        this.drawBackground();
     }
 
 }
---
+++ src/app/theme/themes.ts
@@ -0,0 +1,10 @@
+import { Color } from 'paper';
+
+export const West = {
+    // https://coolors.co/eee2df-eed7c5-c89f9c-c97c5d-b36a5e
+    platinum    : new Color('#EEE2DF'),
+    almond      : new Color('#EED7C5'),
+    tuscany     : new Color('#C89F9C'),
+    copperRed   : new Color('#C97C5D'),
+    copperPenny : new Color('#B36A5E'),
+};
